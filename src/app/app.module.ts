3ea87a0d1dedc249cc206e9a601140f1e1e92f34
Cem Caylak <cem.caylak@adesso.de>
2020-07-14 17:00:58 +0200

    Fix location of HeaderComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {TokenInterceptor} from './token-interceptor';
 import {AppRoutingModule, routingComponents} from './app-routing.module';
 import {AppComponent} from './app.component';
-import {HeaderComponent} from './header/header.component';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
@@ -16,7 +15,6 @@ import {PaginationModule} from 'ngx-bootstrap/pagination';
 import {ReactiveFormsModule} from '@angular/forms';
 import {OAuthModule} from 'angular-oauth2-oidc';
 import {NotificationComponent} from './notification/notification.component';
-import {AuthGuard} from "./services/auth.guard";
 
 @NgModule({
   declarations: [
@@ -49,7 +47,7 @@ import {AuthGuard} from "./services/auth.guard";
     useClass: TokenInterceptor,
     multi: true
   }],
-  bootstrap: [AppComponent, HeaderComponent],
+  bootstrap: [AppComponent],
 })
 
 export class AppModule {
